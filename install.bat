5b0e9dd4eb709dc56c97fb0b66088c1ea784ce68
nicolus <bluescreenjunky@gmail.com>
2023-11-18 10:00:43 +0100

    Wait for 2 seconds before restarting wsl after install is complete

--- install.bat
+++ install.bat
@@ -1,7 +1,12 @@
 @ECHO OFF
+
 set SSH_PATH=%USERPROFILE%\.ssh
 set WSLENV=%WSLENV%:SSH_PATH
 
 wsl -e bash -li -c ./scripts/install.sh
 wsl --shutdown
+TIMEOUT /T 2 /NOBREAK
+wsl exit
+wsl --shutdown
+TIMEOUT /T 2 /NOBREAK
 wsl
